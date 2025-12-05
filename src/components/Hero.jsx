c083974d5d397438db7acc5c89da2ae22ea267d7
Jingneral <gc.deliarte@gmail.com>
2025-12-05 17:54:10 +0800

    Edit the Profile/Hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import profile from "../assets/profilehero.png";
 const Hero = () => {
   return (
     <section id="hero" className="hero-wrapper">
+      {/* TEXT SECTION */}
       <div className="hero-left">
         <p className="hero-intro">Hello Everyone, I Am</p>
 
@@ -15,13 +16,15 @@ const Hero = () => {
         <p className="hero-role">4th Year - Web Developer</p>
 
         <div className="hero-buttons">
-          <a href="#contact" className="contact-btn">Contact Me</a>
+          <a href="#contact" className="contact-btn">
+            Contact Me
+          </a>
         </div>
       </div>
 
+      {/* IMAGE SECTION */}
       <div className="hero-right">
         <div className="circle-bg"></div>
-
         <img src={profile} alt="General Carlo Deliarte" className="profile-img" />
       </div>
     </section>
